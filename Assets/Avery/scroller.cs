623206397c15af37d10f4e2d63de9b67015dd198
Goldragon <goldragon227@gmail.com>
2017-11-05 15:58:31 -0800

    Add Avery Assets

---
+++ Assets/Avery/scroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroller : MonoBehaviour
+{
+    public float Speed;
+
+    private void Start()
+    {
+        transform.Translate(new Vector3()
+        {
+            y = -400
+        });
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3()
+        {
+            y = Time.deltaTime * Speed
+        });
+    }
+}
