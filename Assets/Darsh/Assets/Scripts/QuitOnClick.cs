730909a6eef036b979679bc571374876dfa2edf1
Darsh Lin <darshlin714@gmail.com>
2017-11-05 15:25:49 -0800

    Start Screen

---
+++ Assets/Darsh/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitOnClick : MonoBehaviour
+{
+
+    public Button quitButton;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = quitButton.GetComponent<Button>();
+        btn.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
