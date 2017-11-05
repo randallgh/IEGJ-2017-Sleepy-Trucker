806693b864b915f265ad8e54fd53660d86b673a6
Goldragon <goldragon227@gmail.com>
2017-11-04 22:40:11 -0700

    Added Xander Assests

---
+++ Assets/Xander/Assets/ObstacleScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour {
+
+    public float maxWakefulness = 100;
+    public float realChanceMod = 0;
+
+}
